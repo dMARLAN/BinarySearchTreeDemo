8744641ee6d307c48458de9ff0786c15078d808e
MARLAN <cpenarsky@shaw.ca>
2022-11-01 14:43:22 -0500

    initial commit

---
+++ src/main/java/org/example/BinarySearchTreeDemo.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Random;
+
+public class BinarySearchTreeDemo {
+
+    public static void main(String[] args) {
+        int sizeOfTree = 10;
+        Random random = new Random();
+
+        Node root = new Node(5);
+
+        for (int i = 0; i < sizeOfTree; i++) {
+            // root.insert(random.nextInt(0,100));
+            root.insert(i);
+        }
+
+        System.out.println("In Order:"); // LEFT ROOT RIGHT | 0 1 2 3 4 5 6 7 8 9
+        root.printInOrder();
+        System.out.println("-----------------");
+
+        System.out.println("Post Order:"); // LEFT RIGHT ROOT | 0 1 2 3 4 6 7 8 9 5
+        root.printPostOrder();
+        System.out.println("-----------------");
+
+        System.out.println("Pre Order:"); // ROOT LEFT RIGHT | 5 0 1 2 3 4 6 7 8 9
+        root.printPreOrder();
+        System.out.println("-----------------");
+    }
+}
---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,75 @@
+package org.example;
+
+public class Node {
+    Node left;
+    Node right;
+    Integer data;
+
+    public Node(Integer data) {
+        this.data = data;
+    }
+
+    public void insert(int value) {
+        if (value <= data) {
+            if (left == null) {
+                left = new Node(value);
+            } else {
+                left.insert(value);
+            }
+        } else {
+            if (right == null) {
+                right = new Node(value);
+            } else {
+                right.insert(value);
+            }
+        }
+    }
+
+    public boolean contains(int value) {
+        if (value == data) {
+            return true;
+        } else if (value < data) {
+            if (left == null) {
+                return false;
+            } else {
+                return left.contains(value);
+            }
+        } else {
+            if (right == null) {
+                return false;
+            } else {
+                return right.contains(value);
+            }
+        }
+    }
+
+    public void printInOrder() {
+        if (left != null) {
+            left.printInOrder();
+        }
+        System.out.println(data);
+        if (right != null) {
+            right.printInOrder();
+        }
+    }
+
+    public void printPostOrder() {
+        if (left != null) {
+            left.printPostOrder();
+        }
+        if (right != null) {
+            right.printPostOrder();
+        }
+        System.out.println(data);
+    }
+
+    public void printPreOrder() {
+        System.out.println(data);
+        if (left != null) {
+            left.printPreOrder();
+        }
+        if (right != null) {
+            right.printPreOrder();
+        }
+    }
+}
